640337235b12ecf4bfda7a348be578cd3b1a6af7
mctiantian2501314 <100036451+mctiantian2501314@users.noreply.github.com>
2024-12-11 11:45:33 +0800

    Update main.py
    提交

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from public.usage import USAGE as html
+from api.bqxs520 import router as bqxs520_router
 from api.hello import router as hello_router
 from api.random import router as random_router
 from api.v1.groq import router as groq_router
@@ -10,6 +11,7 @@ app = FastAPI()
 app.include_router(hello_router, prefix="/hello")
 app.include_router(random_router, prefix="/random")
 app.include_router(groq_router, prefix="/v1/groq")
+app.include_router(bqxs520_router, prefix="/bqxs520")
 
 
 @app.get("/")
