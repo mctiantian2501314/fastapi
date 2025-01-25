e1eeaee6badf3e52a9e00984494005cc4a42e9d4
mctiantian2501314 <100036451+mctiantian2501314@users.noreply.github.com>
2025-01-26 02:59:10 +0800

    Update main.py
    新增路由69hsz

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-from public.usage import USAGE as html
-from api.bqxs520 import router as bqxs520_router
 from fastapi import FastAPI
-from fastapi.responses import Response
+from fastapi.responses import Response, JSONResponse
+from public.usage import USAGE as html
+
+from api.bqxs520 import router as bqxs520_router
+from api.six_nine_hsz import router as hsz_router  
+
 app = FastAPI()
 
 
-
 app.include_router(bqxs520_router, prefix="/bqxs520")
-
+app.include_router(hsz_router, prefix="/69hsz") 
 
 @app.get("/")
 def _root():
