2d2e8db5d5b80c20828362ba72cd4d5e6e169bea
mctiantian2501314 <100036451+mctiantian2501314@users.noreply.github.com>
2025-03-11 01:06:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from public.usage import USAGE as html
 from api.hello import router as hello_router
 from api.bqxs520 import router as bqxs520_router
 from api.six_nine_hsz import router as hsz_router
-
+from api.upload_to_github import router as upload_router  
 
 app = FastAPI()
 
@@ -15,7 +15,7 @@ app = FastAPI()
 app.include_router(hello_router, prefix="/hello")
 app.include_router(bqxs520_router, prefix="/bqxs520")
 app.include_router(hsz_router, prefix="/69hsz")
-
+app.include_router(upload_router, prefix="/github")  
 @app.get("/")
 def _root():
     return Response(content=html, media_type="text/html")
