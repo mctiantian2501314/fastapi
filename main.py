8559720a8b330244be98d3fe873ff94af3d45b53
mctiantian2501314 <100036451+mctiantian2501314@users.noreply.github.com>
2025-03-23 22:11:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ from api.upload_to_github import router as upload_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有头部
+)
 
 app.include_router(hello_router, prefix="/hello")
 app.include_router(bqxs520_router, prefix="/bqxs520")
@@ -19,3 +26,9 @@ app.include_router(upload_router, prefix="/github")
 @app.get("/")
 def _root():
     return Response(content=html, media_type="text/html")
+@app.get("/proxy")
+async def proxy(request: Request):
+    target_url = "https://ttdndd.serv00.net/cf.php"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(target_url)
+        return JSONResponse(content=response.json(), media_type="application/json")
